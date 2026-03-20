42e98e6e69905ad2f4bfae8f71397d9442a83c1a
juYoung <jwawa129@gmail.com>
2026-03-20 17:54:30 +0900

    flask_crud,rest pandas Basic, XML출력

--- pro4flask/fpro17rest/app.py
+++ pro4flask/fpro17rest/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from db import get_connFunc
 
-app= Flask(__name__)
+app = Flask(__name__)
 
 @app.get("/")
 def home():
@@ -11,62 +11,68 @@ def home():
 @app.get("/api/jikwon")
 def jikwon_list():
     sql = """
-    select jikwonno, jikwonname, busername, jikwonjik, 
-    jikwonpay, year(jikwonibsail) as ibsayear
-    from jikwon
-    inner join buser on jikwon.busername = buser.buserno
-    order by jikwonno
+        select jikwonno,jikwonname,busername,jikwonjik,jikwonpay,year(jikwonibsail) as ibsayear
+        from jikwon
+        inner join buser on jikwon.busernum=buser.buserno
+        order by jikwonno
     """
 
     with get_connFunc() as conn:
         with conn.cursor() as cur:
             cur.execute(sql)
             rows = cur.fetchall()
-    return jsonify({"ok": True,"data":rows})
+
+    return jsonify({"ok":True, "data":rows})
+
 
 # 직원 1명 조회
-@app.get("/api/jikwon/<int:on>")
+@app.get("/api/jikwon/<int:no>")
 def jikwon_one(no):
     sql = """
-    select jikwonno, jikwonname, busername, jikwonjik, 
-    jikwonpay, year(jikwonibsail) as ibsayear
-    from jikwon
-    inner join buser on jikwon.busername = buser.buserno
-    where jikwonno = %s
+        select jikwonno,jikwonname,busername,jikwonjik,jikwonpay,year(jikwonibsail) as ibsayear
+        from jikwon
+        inner join buser on jikwon.busernum=buser.buserno
+        where jikwonno=%s
     """
 
     with get_connFunc() as conn:
         with conn.cursor() as cur:
             cur.execute(sql, (no,))
-            rows = cur.fetchone()
-    return jsonify({"ok": True,"data":rows})
+            row = cur.fetchone()
+
+    return jsonify({"ok":True, "data":row})
+
 
 # 전체 부서 조회
 @app.get("/api/buser")
-def jikwon_list():
+def buser_list():
     sql = """
-    select * from buser order by buserno """
+        select * from buser order by buserno
+    """
 
     with get_connFunc() as conn:
         with conn.cursor() as cur:
             cur.execute(sql)
             rows = cur.fetchall()
-    return jsonify({"ok": True,"data":rows})
+
+    return jsonify({"ok":True, "data":rows})
 
 
 # 특정 부서 직원 조회
-@app.get("/api/jikwon/<int:bon>/jikwon")
+@app.get("/api/buser/<int:bno>/jikwon")
 def buser_jikwon(bno):
     sql = """
-    select jikwonno, jikwonname, jikwonjik, 
-    jikwonpay, year(jikwonibsail) as ibsayear
-    from jikwon where busernnum = %s
+        select jikwonno,jikwonname,jikwonjik,jikwonpay,year(jikwonibsail) as ibsayear
+        from jikwon
+        where busernum=%s
     """
+
     with get_connFunc() as conn:
         with conn.cursor() as cur:
-            cur.execute(sql)
+            cur.execute(sql, (bno,))
             rows = cur.fetchall()
-    return jsonify({"ok": True,"data":rows})
+
+    return jsonify({"ok":True, "data":rows})
 
 
 if __name__ == "__main__":
