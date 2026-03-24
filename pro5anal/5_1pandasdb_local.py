f0f0acb3e9f450d5c3769b52b7e171401a6da15f
juYoung <jwawa129@gmail.com>
2026-03-24 18:17:29 +0900

    db_data연동및 그래프그리기

--- pro5anal/5_1pandasdb_local.py
+++ pro5anal/5_1pandasdb_local.py
@@ -1,5 +1,5 @@
 """
-local db 연동 후 DataFrame에 자료 저장
+local DB 연동 후 DataFrame에 자료 저장 - sqlite3사용
 """
 # try문 생략
 import sqlite3
@@ -26,11 +26,34 @@ for a in rows:
         print(j)
 print()
 
-print('-'*15,'rows를 DataFrame에 저장','-'*15)
+print('-'*15,'rows를 DataFrame에 저장1 - fetchall','-'*15)
 import pandas as pd
 df1 = pd.DataFrame(rows, columns=['product','maker','weight','price'])
 print(df1)
 print(df1.describe())
+print()
+
+print('-'*15,'rows를 DataFrame에 저장2 - read_sql()','-'*15)
+df2 = pd.read_sql("select * from extab", conn) # sql문 연결객체 conn
+print(df2)
+print(pd.read_sql('select count(*) as 건수 from extab', conn))
+print()
+
+print('-'*15,'DataFrame의 자료를 테이블에 저장(insert) - to_sql','-'*15)
+data = {
+    'product':['연필','볼펜','지우개'],
+    'maker':['모나미','모나미','모나미'],
+    'weight':[2.3, 3.0, 5.0],
+    'price':(1000, 2000, 500)
+}
+frame = pd.DataFrame(data)
+# print(frame)
+# auto commit
+frame.to_sql('extab', conn, if_exists='append', index=False) #if_exists있으면 추가(append)
+df3 = pd.read_sql("select * from extab", conn) # sql문 연결객체 conn
+print(df3)
+
+
 
 cursor.close()
 conn.close()
