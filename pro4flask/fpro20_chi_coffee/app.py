f96876295b12324733eb501afb8e843517b13d25
juYoung <jwawa129@gmail.com>
2026-03-30 14:51:42 +0900

    chi제곱검정 일원,이원

---
+++ pro4flask/fpro20_chi_coffee/app.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, request
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import koreanize_matplotlib
+import seaborn as sns
+import pymysql
